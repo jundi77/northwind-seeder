6b68dae35f89f0a28dd0c8235e4e599f6dbb4a44
Jundullah H. R <jundihanif77@gmail.com>
2021-06-21 14:55:27 +0700

    initial commit

---
+++ app/Models/CustomerCustomerDemo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerCustomerDemo extends Model
+{
+    use HasFactory;
+
+    protected $table = "customer_customer_demo";
+    protected $primaryKey = [
+        'customer_id',
+        'customer_type_id',
+    ];
+}
---
+++ app/Models/EmployeeTerritory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeeTerritory extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = [
+        'employee_id',
+        'territory_id'
+    ];
+}
